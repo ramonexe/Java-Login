54e0898746ebb69337dc0a1c38d054b0ac6884c8
Ramon Garcia <143695500+ramonexe@users.noreply.github.com>
2023-09-28 15:08:00 -0300

    Add files via upload
    Aprendendo a usar arraylist

--- loginproject/src/Login.java
+++ loginproject/src/Login.java
@@ -5,6 +5,7 @@ public class Login {
     public static void main() {
 
         Main main = new Main();
+        String opcoes;
         int opcao;
         String usuario, senha, user = null, pass = null;
         Scanner input = new Scanner(System.in);
@@ -12,21 +13,55 @@ public class Login {
         do {
 
             System.out.println("1 - Login\n2 - Register\n3 - Quit");
-            opcao = input.nextInt();
+            opcoes = input.next();
+            switch (opcoes) {
+
+                case "1" :
+                    do {
+                        System.out.println("<< Login >>\n1 - voltar\nUser: ");
+                        usuario = input.next();
+                        System.out.println("Password: ");
+                        senha = input.next();
+                        if ((Objects.equals(usuario, user)) && (Objects.equals(pass, senha))) {
+                            opcao = 3;
+                            Main.inicio();
+                        } else if ((!Objects.equals(usuario, user)) || (!Objects.equals(pass, senha))) {
+                            System.out.println("User or Password invalid, try again.\n");
+                        }
+                    } while ((!Objects.equals(usuario, user)) || (!Objects.equals(pass, senha)));
+                    break;
+
+                case "2" :
+                    System.out.println("<< Register >>\n1 - voltar\nUser: ");
+                    user = input.next();
+                    System.out.println("Password: ");
+                    pass = input.next();
+                    break;
+
+                case "3" :
+                    System.out.println("ENCERRANDO...");
+                    break;
+
+                default:
+                    System.out.println("Invalid Option, try again.");
+
+            }
+            /*O LOGIN COM IF E ELSE FICOU MUITO BAGUNÇADO E ALTEREI PARA O SWITCH CASE, PORÉM OS DOIS DA PRA UTILIZAR
+            MAS O IF E ELSE DEVE USAR MUITA MEMÓRIA.
+
             if (opcao > 3) {
                 System.out.println("Invalid Option, try again.");
             } else if (opcao == 2) {
-                System.out.println("<< Register >>\nUser: ");
+                System.out.println("<< Register >>\n1 - voltar\nUser: ");
                 user = input.next();
                 System.out.println("Password: ");
                 pass = input.next();
             } else if (opcao == 1) {
                 do {
-                    System.out.println("<< Login >>\nUser: ");
+                    System.out.println("<< Login >>\n1 - voltar\nUser: ");
                     usuario = input.next();
                     System.out.println("Password: ");
                     senha = input.next();
-
                     if ((Objects.equals(usuario, user)) && (Objects.equals(pass, senha))) {
                         opcao = 3;
                         Main.inicio();
@@ -36,7 +71,7 @@ public class Login {
 
                 } while ((!Objects.equals(usuario, user)) || (!Objects.equals(pass, senha)));
             }
-
-        } while ((opcao != 3));
+*/
+        } while (!opcoes.equals(String.valueOf(3)));
     }
 }
--- loginproject/src/Main.java
+++ loginproject/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Login login = new Login();
@@ -5,6 +8,55 @@ public class Main {
     }
 
     public static void inicio() {
-        System.out.println("Logged!");
+
+        ArrayList<String> produtos = new ArrayList<String>();
+        ArrayList<Integer> codigos = new ArrayList<Integer>();
+        String opcao = null;
+        System.out.println("\nLogged!\n");
+        Scanner input = new Scanner(System.in);
+            do {
+                System.out.println("---COOPSTOCK MENU---\n" +
+                        "1 - CADASTRAR PRODUTO\n" +
+                        "2 - CONSULTAR PRODUTO\n" +
+                        "3 - ENTRADA DE PRODUTOS\n" +
+                        "4 - SAIDA DE PRODUTOS\n" +
+                        "5 - RELATÓRIO GERAL\n" +
+                        "6 - sair\n");
+                opcao = input.next();
+
+                switch (opcao) {
+                    case "1":
+                        System.out.println("\n--CADASTRAR PRODUTO--\n" +
+                                "Nome do Produto: ");
+                        produtos.add(input.next());
+                        System.out.println("Código do Produto: ");
+                        codigos.add(input.nextInt());
+                        break;
+                    case "2":
+                        System.out.println("\n--CONSULTAR PRODUTO--\n");
+                        break;
+                    case "3":
+                        System.out.println("\n--ENTRADA DE PRODUTOS--\n");
+                        break;
+                    case "4":
+                        System.out.println("\n--SAIDA DE PRODUTOS--\n");
+                        break;
+                    case "5":
+                        System.out.println("\n--RELATÓRIO GERAL--\n");
+                        for (String todosProdutos : produtos) {
+                            System.out.println(todosProdutos);
+                        }
+                        System.out.println("\nInsira qualquer botão para voltar\n");
+                        input.next();
+                        break;
+                    case "6":
+                        System.out.println("COOPSTOCK ENCERRANDO...");
+                        Login.main();
+                        break;
+                    default:
+                        System.out.println("Você não escolheu nenhuma opção");
+                        break;
+                }
+            } while (!opcao.equals(String.valueOf(6)));
+        }
     }
-}
